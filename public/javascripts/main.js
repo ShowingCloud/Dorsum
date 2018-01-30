8790a8b32911e42c5411759b75daf50b7ceb4afe
WANG Guoqin @rwgq <wangguoqin1001@gmail.com>
2018-01-30 07:21:23 +0000

    Mobile version added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,34 +1,59 @@
-var animateObj = $(".animations");
-var step = 0;
+$(document).ready(function(){
+
+    //移动端代码
+    if(navigator.userAgent.match(/mobile/i)) {
+        calScreen();
+    }else{
+        document.documentElement.style.fontSize = '';
+    }
+
+    //计算手机宽度
+    function calScreen(){
+        var deviceWidth = document.documentElement.clientWidth;
+        if(deviceWidth > 750) deviceWidth = 750;
+        document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
+    }
 
 
-setInterval(function(){
-  if(step>=3){
-    step = 0;
-  }
-  animateObj.eq(step).css('color','#f8bf4b');
-  animateObj.eq(step).siblings().css('color','#000');
-  $('.mainComputer').attr('src','images/computer'+(step+1)+'.gif');
-  step++;
-},2000)
+
+    var animateObj = $(".animations");
+    var step = 0;
 
 
-$(".lesson_tip").hover(function(){
-    $(".lesson_tip span").css('color','#f8bf4b');
-},function(){
-    $(".lesson_tip span").css('color','#9f9e9d');
+    setInterval(function(){
+        if(step>=3){
+            step = 0;
+        }
+        animateObj.eq(step).css('color','#f8bf4b');
+        animateObj.eq(step).siblings().css('color','#000');
+        $('.mainComputer').attr('src','/images/computer'+(step+1)+'.gif');
+        step++;
+    },2000)
+
+
+    $(".lesson_tip").hover(function(){
+        $(".lesson_tip span").css('color','#f8bf4b');
+    },function(){
+        $(".lesson_tip span").css('color','#9f9e9d');
+    });
+
+
+    $(".tool_tip").hover(function(){
+        $(".tool_tip span").css('color','#f8bf4b');
+    },function(){
+        $(".tool_tip span").css('color','#9f9e9d');
+    });
+
+
+    $(".logo").hover(function(){
+        $(".logo").attr('src','/images/logo1.png');
+    },function(){
+        $(".logo").attr('src','/images/logo.png');
+    });
+
+
 });
 
 
-$(".tool_tip").hover(function(){
-    $(".tool_tip span").css('color','#f8bf4b');
-},function(){
-    $(".tool_tip span").css('color','#9f9e9d');
-});
 
 
-$(".logo").hover(function(){
-    $(".logo").attr('src','./images/logo1.png');
-},function(){
-    $(".logo").attr('src','./images/logo.png');
-});
